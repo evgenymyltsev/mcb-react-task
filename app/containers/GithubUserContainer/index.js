2d6453902a45ce4934f9dbb7a6ed44be82632061
Evgeny Myltsev <myltsev.ea@gmail.com>
2017-09-24 21:14:40 +0300

    firs experience with redux-saga looks bad but work :)

--- app/containers/GithubUserContainer/index.js
+++ app/containers/GithubUserContainer/index.js
@@ -12,9 +12,13 @@ import { withState } from 'recompose';
 import makeSelectGithubUserContainer from './selectors';
 import * as actions from './actions';
 
-const SearchComponentStateless = ({ searchText, setSearchText, loadUser }) => <div>
-  <input type="text" value={searchText} onChange={(event) => setSearchText(event.target.value)} />
-  <button onClick={() => loadUser(searchText)}>Search</button>
+const SearchComponentStateless = ({ searchText, setSearchText, loadUser }) => <div className="mdl-grid">
+  <input className="mdl-cell mdl-cell--4-col mdl-textfield__input" type="text" value={searchText} onChange={(event) => setSearchText(event.target.value)} />
+<button className="mdl-cell mdl-cell--2-col mdl-button mdl-button--raised mdl-button--primary" onClick={() => loadUser(searchText)}>Search</button>
+</div>
+
+const CommitsList = ({loadCommits}) => <div>
+
 </div>
 
 const SearchComponent = withState('searchText', 'setSearchText', 'giko')(SearchComponentStateless);
@@ -23,8 +27,31 @@ export class GithubUserContainer extends React.PureComponent { // eslint-disable
   render() {
     return (
       <div>
-        <SearchComponent loadUser={this.props.loadUser} />
-        {this.props.githubUserContainer.userRepos && this.props.githubUserContainer.userRepos.map((repo) => <div key={repo.id}>{repo.name}</div>)}
+        <div className="mdl-cell mdl-cell--6-col" >
+          <SearchComponent loadUser={this.props.loadUser} />
+        <div className="mdl-grid">
+          <ul className="mdl-list mdl-cell mdl-cell--6-col">
+            { this.props.githubUserContainer.userRepos &&
+              this.props.githubUserContainer.userRepos.map((repo) => {
+                return <li  className="mdl-list__item" key={repo.id}
+                            onClick={() => this.props.loadCommits(repo.full_name)}>
+                            {repo.name}
+                       </li>
+              })}
+          </ul>
+          <ol className="mdl-cell mdl-cell--6-col">
+            { this.props.githubUserContainer.userCommits &&
+              this.props.githubUserContainer.userCommits.map((commit, index) => {
+                return <li key={index}>
+                  <a href={commit.html_url}
+                    target="_blank">
+                    {commit.sha}
+                  </a>
+                </li>
+              })}
+          </ol>
+        </div>
+        </div>
       </div>
     );
   }
@@ -41,7 +68,14 @@ const mapStateToProps = createStructuredSelector({
 function mapDispatchToProps(dispatch) {
   return {
     dispatch,
-    loadUser: (userName) => dispatch(actions.loadUser(userName)),
+    loadUser: (userName) => {
+      if (userName.search(/[А-яЁё]/) === -1 ) {
+        dispatch(actions.loadUser(userName))
+      } else {
+        alert('Допускаются только англиские буквы')
+      }
+    },
+    loadCommits: (repoName) => dispatch(actions.loadCommits(repoName))
   };
 }
 
--- app/containers/GithubUserContainer/sagas.js
+++ app/containers/GithubUserContainer/sagas.js
@@ -4,19 +4,29 @@ import { request, postRequest } from 'utils/request';
 import * as actions from './actions';
 
 // Individual exports for testing
-export function* loadUserSaga(action) {
-  const result = yield call(request, `users/${action.payload.userName}/repos`);
-  yield put(actions.userLoaded(result));
+export function* loadDataSaga(action) {
+  if (action.payload.hasOwnProperty('userName')) {
+    try {
+      const result = yield call(request, `users/${action.payload.userName}/repos`);
+      yield put(actions.userLoaded(result));
+    } catch (error) {
+      alert(`Похоже, пользователь ${action.payload.userName} не существует.`)
+    }
+  } else if (action.payload.hasOwnProperty('repoName')) {
+    const result = yield call(request, `repos/${action.payload.repoName}/commits`);
+    yield put(actions.commitsLoaded(result.slice(0,10)));
+  }
+
 }
 
 export function* defaultSaga() {
   while (true) {
-    const action = yield take(actions.loadUser);
-    yield call(loadUserSaga, action);
+    const user = yield take(actions.loadUser);
+    yield call (loadDataSaga, user);
   }
 }
 
 // All sagas to be loaded
 export default [
-  defaultSaga,
+  defaultSaga
 ];
